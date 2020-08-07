c3175e4af99ac0481497ac4218efa86bf7f40421
Tammy Crittenden <tcritt71@gmail.com>
2020-08-07 14:50:46 -0500

    react version

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Game from './Component/game/Game';
+import bulldogs from './assets/images/bulldogs.jpg';
+import eagles from './assets/images/eagles.jpg';
+import wildcats from './assets/images/eagles.jpg';
+import yellowjackets from './assets/images/yellowjackets.jpg'
 
-function App() {
+function App(props) {
+  const bulldog = {
+    name: 'Bulldogs',
+    logoSrc: bulldogs
+  }
+
+  const wildcat = {
+    name: 'Wildcats',
+    logoSrc: wildcats
+  }
+
+  const eagle = {
+    name: 'Eagles',
+    logoSrc: eagles
+  }
+
+  const yellowjacket = {
+    name: 'Yellow Jackets',
+    logoSrc: yellowjackets
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Game
+        venue="Union 525 Gem"
+        homeTeam={bulldog}
+        visitingTeam={wildcat}
+      />
+      <Game
+        venue="Sheridan Arena"
+        homeTeam={eagle}
+        visitingTeam={yellowjacket}
+      />
     </div>
-  );
+  )
 }
 
-export default App;
+
+export default App
